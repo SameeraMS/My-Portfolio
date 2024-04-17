ecf0b19c333abba84b2d9294c52750bcd1b1a8e3
Sameera Madushan Silva <madushansameera499@gmail.com>
2024-04-17 10:14:47 +0530

    add animation

--- assests/script/script.js
+++ assests/script/script.js
@@ -51,3 +51,19 @@ window.addEventListener('scroll', function() {
         scrollingPage.classList.remove('open');
     }
 });
+
+
+
+//animation
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+const hiddenElements = document.querySelectorAll('.hidden-left, .hidden-right, .hidden-top, .hidden-bottom');
+hiddenElements.forEach((e1)=>observer.observe(e1));
